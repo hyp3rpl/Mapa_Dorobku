78ef4f0837b15f92622b164420ad308b53806a66
chambii <chalupka.janek@gmail.com>
2026-03-17 11:48:00 +0100

    comit 2 grupa

--- core/src/App.jsx
+++ core/src/App.jsx
@@ -3,12 +3,14 @@ import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import EpokiPage from '../../grupa-1/EpokiPage'
 import MapaDorobku from '../../grupa-3/MapaDorobku'
+import Asyn from '../../grupa-2/Asyn'
 
 function App() {
   const [activeGroup, setActiveGroup] = useState(null)
 
   const renderContent = () => {
     if (activeGroup === 1) return <EpokiPage />
+    if (activeGroup === 2) return <Asyn />
     if (activeGroup === 3) return <MapaDorobku />
     return <Home />
   }
